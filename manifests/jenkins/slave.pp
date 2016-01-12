f83855be1db4f65d3c6aef21ab73964a7dccead3
Steven Nemetz <steven.nemetz@fireeye.com>
2016-01-12 11:45:09 -0800

    Testing lint plugin

--- manifests/jenkins/slave.pp
+++ manifests/jenkins/slave.pp
@@ -3,7 +3,10 @@
 #
 # Create a Jenkins build slave
 #
+# FIX:
+# Lint roles and profile plugin needs to be fixed to support ::profiles as well as profiles
+#
 class roles::jenkins::slave {
-  include profiles::base
+  include ::profiles::base
   include ::profiles::jenkins::slave
 }
